76cd8bc08c3a2689b53c9b04eb258da8dcddc78b
Sueneé <info@easyweby.cz>
2026-08-18 21:51:36 +0200

    Wire redesigned project update dialog

--- managed_builder_runner.py
+++ managed_builder_runner.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 import managed_builder
 import project_update
+import project_update_dialog
 import ui_windows
 
 _base_create_initial_timeline=managed_builder.create_initial_timeline
@@ -14,6 +15,7 @@ def _center_above_resolve(root):
 
 
 managed_builder.center=_center_above_resolve
+project_update.ask=project_update_dialog.ask
 project_update.set_timeline_creator(_base_create_initial_timeline)
 managed_builder.build=project_update.build
 
